43b12297f6a50e670793fe54df95b94593c4e6b9
ymc9 <yiming@whimslab.io>
2025-08-09 21:53:55 +0800

    update

--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
 import { createClient } from './db';
+import { createUsersAndPosts } from './utils';
 
 async function main() {
   const db = await createClient();
+  await createUsersAndPosts(db);
 
+  console.log('Delete a unique post');
+  console.log(await db.post.delete({ where: { id: 1 } } ));
+
+  console.log('Delete many posts');
+  console.log(await db.post.deleteMany({ where: { published: false } }));
 }
 
 main();
